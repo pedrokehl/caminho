d601c49873944c1dc07642c6be347dd5fbb43a91
Pedro Kehl <pedro.kehl.br@socialbakers.com>
2024-03-10 16:30:45 +0100

    Improve documentation

--- src/types.ts
+++ src/types.ts
@@ -6,13 +6,39 @@ import type { InternalOnStepFinished } from './utils/onStepFinished'
 import type { InternalOnStepStarted } from './utils/onStepStarted'
 
 export interface Caminho {
+  /**
+  * receives an array of StepFunctions and each provided step has the same parameters and behavior as a pipe.
+  * Useful only for Asynchronous operations.
+   */
   pipe: (pipeParams: PipeGenericParams) => this
+  /**
+  * receives an array of StepFunctions and each provided step has the same parameters and behavior as a pipe.
+  * Useful only for Asynchronous operations.
+   */
   parallel: (multiPipeParams: PipeGenericParams[]) => this
   filter: (predicate: FilterPredicate) => this
   reduce: <T>(reduceParams: ReduceParams<T>) => this
 
   run(initialBag: ValueBag, pickLastValues: string[]): Promise<unknown>
   run(initialBag?: ValueBag): Promise<undefined>
+}
+
+export type BasePipe = {
+  /**
+  * The name of the property to be assigned to the cumulate context.
+  * The value of the property is the returned value from the step.
+  */
+  provides?: string
+  /**
+  * Name of the step, useful when logging the steps
+  */
+  name?: string
+  /**
+  * Concurrency is unlimited by default, it means a step can be run concurrently as many times as the flow produces
+  * You can limit the concurrency by using the `maxConcurrency` property.
+  * This is useful for example when you are calling an API that can't handle too many concurrent requests.
+   */
+  maxConcurrency?: number
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -39,7 +65,19 @@ export type OnStepFinishedParams = {
 export type Loggers = { onStepFinished: InternalOnStepFinished; onStepStarted: InternalOnStepStarted }
 
 export type CaminhoOptions = {
+  /**
+  * Callback to execute for each step execution start.
+  * Useful for logging.
+  */
   onStepStarted?: OnStepStarted
+  /**
+  * Callback to execute for each step execution end.
+  * Useful for logging.
+  */
   onStepFinished?: OnStepFinished
+  /**
+  * Number of items allowed to be in the flow in any given point in time.
+  * Useful for applying backpressure and making sure the flow won't produce data faster than it can consume.
+  */
   maxItemsFlowing?: number
 }
